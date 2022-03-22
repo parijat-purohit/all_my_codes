30deea1ffd298846bbe02d47d8f187c5fdd93c65
parijat0904114 <parijat2009@gmail.com>
2022-03-22 00:12:12 -0600

    Create BINARY_SEARCH_TREE.py

---
+++ BINARY_SEARCH_TREE.py
@@ -0,0 +1,23 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def insert(self, num):
+        if self.val == 0:
+            return self.val = num
+        if num < self.val:
+            if self.left:
+                return self.left.insert(num)
+            self.left = TreeNode(num)
+        elif num > self.val:
+            if self.right:
+                return self.right.insert(num)
+            self.right = TreeNode(num)
+
+
+nums = [5,4,3,2,1]
+bst = TreeNode()
+for num in nums:
+    bst.insert(num)

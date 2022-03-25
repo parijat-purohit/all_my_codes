f8470bf2bf7a6f756d4ac982e8a442cec60c7e08
parijat0904114 <parijat2009@gmail.com>
2022-03-24 19:53:54 -0600

    Create uva_10071.py

---
+++ uva_10071.py
@@ -0,0 +1,11 @@
+"""
+https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1012
+"""
+import sys
+while True:
+        try:
+            inp = input()
+            st = inp.split(' ')
+            print(2*int(st[0])*int(st[1]))
+        except:
+            break
